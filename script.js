dbca457f4ea98cac1b98d9c500f39b8ee4d404d5
SkyIsBlue <patrickstat022@gmail.com>
2024-02-19 21:54:35 +0530

    Files

---
+++ script.js
@@ -0,0 +1,34 @@
+const API_KEY = "c80821be1a2287cd32383ad92c1e3655";
+
+const locatione = document.getElementById("locatione");
+const temperature = document.getElementById("temperature");
+const city = document.getElementById("city");
+const p = document.getElementById("p");
+p.style.visibility="hidden";
+
+function searchCity() {
+    const cityName = city.value;
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + API_KEY)
+        .then(function (resp) { return resp.json() })
+        .then(function (data) {
+            console.log(data);
+            if (!data.name) {
+                p.style.visibility = "visible";
+            }
+            else {
+                p.style.visibility="hidden";
+                locatione.innerText = data.name;
+                temperature.innerText = data.main.temp + "F / " + Math.round(parseFloat(data.main.temp) - 273.15) + "°C";
+            }
+        })
+        .catch(function (error) {
+            console.error('Error fetching data:', error);
+        });
+}
+
+const form = document.getElementById("search-form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    searchCity;
+    city.innerHTML = "";
+});
